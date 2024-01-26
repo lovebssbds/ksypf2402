3c787576cc9e12eb0c411397925f2cd0a6fab2d7
lovebssbds <sweetlina.syk@gmail.com>
2024-01-26 12:33:34 +0900

    wheel problem is fixed

--- js/common.js
+++ js/common.js
@@ -225,28 +225,38 @@ $(function(){
    /* *************************************************************************** */
 
    //dbox modal
-   $(".dbox-ani a").each(function(){
-    $(this).click(function(){
+   let $closeScrollTop
+   $(".dbox-ani span").each(function(){
+      $(this).click(function(){
         $(".img-modal").show();
         $(".img-modal-body h2").text( $(this).find("h2").text() );
         $(".img-modal-body .img").html( $(this).find("p").html() );
         $(".img-modal-body .link-close a").attr("href", $(this).find("p img").attr("src"))
         $(".section").off();
-        return false;
-     })
+        $(".img-modal-body .img").scrollTop(0);
+        $('html,body').css('overflow','hidden')
+        $("html,body").scrollTop($scrollTop);
+        $closeScrollTop=$scrollTop;
+        })
     })
-    $(".dbox-slide a").each(function(){
+    $(".dbox-slide span").each(function(){
         $(this).click(function(){
             $(".img-modal").show();
             $(".img-modal-body h2").text( $(this).find("h2").text() );
             $(".img-modal-body .img").html( $(this).find("p").html() );
             $(".img-modal-body .link-close a").attr("href", $(this).find("p img").attr("src"))
             $(".section").off();
-            return false;
+            $(".img-modal-body .img").scrollTop(0);
+            $('html,body').css('overflow','hidden')
+            $("html,body").scrollTop($scrollTop)
+            $closeScrollTop=$scrollTop
         })
     })
     $(".img-modal-close").click(function(){
         $(".img-modal").hide();
+        $('html,body').css('overflow','inherit')
+        $("html,body").scrollTop($closeScrollTop)
         wheel();
     })
+
 })
